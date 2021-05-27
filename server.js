15d778b284263d8058261becfea3631aa121dbef
Kavitha <c.kavitha1987@gmail.com>
2021-05-27 15:35:37 -0400

    make it better

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true,
 useNewUrlParser:true,
 useUnifiedTopology:true,
 }))
-//app.use(express.json())
+app.use(express.json())
 //app.use(cors())
 
 /*app.use('/', (req, res) => {
